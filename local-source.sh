f04ade584c30e99a64f70746543a6da188ca0c36
GSNL-Gayathri <gsnlgayatri104@gmail.com>
2023-10-12 09:20:52 +0530

    mongo-url hidded

--- local-source.sh
+++ local-source.sh
@@ -1,2 +1,2 @@
-MONGO_URL=mongodb+srv://gsnlgayatri104:TVeN1QmEa5uwf8QY@cluster0.boslmzf.mongodb.net/test SECRET_KEY=Usercrud-app-GSNL-Gayathri
+MONGO_URL=mongodb+srv://<username>:<password>@cluster0.boslmzf.mongodb.net/test SECRET_KEY=Usercrud-app-GSNL-Gayathri
 
